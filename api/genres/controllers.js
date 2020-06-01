be463610be05bc5c5895ddd63a36af5d65cc300d
Rasha ali <rashaali84@gmail.com>
2020-06-01 18:52:38 +0200

    genre solution

--- api/genres/controllers.js
+++ api/genres/controllers.js
@@ -14,14 +14,76 @@ const controllers = {
       res.json(rows)
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+
+    const id = req.params.id;
+    const sql = `SELECT * FROM genres where genreId=${Number(id)}`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
+
+  },
   create: (req, res) => {
     // read row data from body
+    const newObjString = JSON.stringify(req.body);
+    // console.log(req.body);
+    const newObj = JSON.parse(newObjString.toLocaleLowerCase());
+
+    console.log(newAlbum);
+    const sql = `INSERT INTO genres (
+                       name
+                   )values("${newObj.name}")`;
+
+    db.run(sql, function (err) {
+      if (err) {
+
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      //return the last id if the table 
+      res.json({ lastID: this.lastID });
+    });
+
   },
   update: (req, res) => {
     // read row data from body
+    const newObjString = JSON.stringify(req.body);
+    // console.log(req.body);
+    const newObj = JSON.parse(newObjString.toLocaleLowerCase());
+    const sql = `UPDATE genres
+    SET 
+     name = "${newObj.name}"
+    WHERE genreId = ${Number(req.params.id)}`;
+
+    db.run(sql, function (err) {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      //return the last id if the table 
+      res.json({ changes: this.changes });
+    });
   },
-  delete: (req, res) => { }
+  delete: (req, res) => {
+
+    const sql = `DELETE FROM genres
+      WHERE genreId = ${Number(req.params.id)}`;
+
+    db.run(sql, function (err) {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      res.json({ deleted: this.changes });
+    });
+  }
 }
 
 module.exports = controllers;

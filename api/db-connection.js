4dde7dbee7035d805f8b8e702c739bc9cfa60b9c
Rasha ali <rashaali84@gmail.com>
2020-06-01 23:22:35 +0200

    changes

--- api/db-connection.js
+++ api/db-connection.js
@@ -2,8 +2,8 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const config = require('../config');
 
-const DB_PATH = path.join(__dirname, '..', process.env.DB_PATH);
+const DB_PATH = path.join(__dirname, '..', config.DB_PATH);
 
-const dbConnection = new sqlite3.Database(DB_PATH);
+const dbConnection = new sqlite3.Database(process.env.DB_PATH);
 
 module.exports = dbConnection;

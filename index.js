6c81c3e29459bdcf4b83c8464871f7c47eb7a7ad
Rasha ali <rashaali84@gmail.com>
2020-06-01 22:39:18 +0200

    changes

--- index.js
+++ index.js
@@ -2,37 +2,33 @@
 
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+
 const config = require('./config');
 const api = require('./api');
-const client = require('./client');
-const app = express();
-app.set('view engine', 'ejs');
-//const port = process.env.PORT || config.PORT;
 
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // log to file
-/*app.use(morgan('combined', {
+app.use(morgan('combined', {
   stream: fs.createWriteStream(
     path.join(__dirname, 'access.log'),
     { flags: 'a' }
   )
-}));*/
-
+}));
 // log to console
 app.use(morgan('dev'));
-//app.use('/', express.static(path.join(__dirname, 'client')));
-app.use('/', (req, res, next) => {
-  res.send({ hi: 'there' })
-});
-//app.use('/chinook', client);
-//app.use('/api', api);
+
+app.use('/', express.static(path.join(__dirname, 'client')));
+
+app.use('/api', api);
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);

6b36ddbcc6c6f820132c96781642f865e9440c02
Rasha ali <rashaali84@gmail.com>
2020-06-01 22:58:37 +0200

    changes

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const config = require('config');
-const api = require('api');
+const config = require('./config');
+const api = require('./api');
 
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -36,9 +34,7 @@ app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).end();
 });
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 app.listen(config.PORT, () => {
   console.log(
     `listening at http://localhost:${config.PORT} (${config.MODE} mode)`

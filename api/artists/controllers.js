b79920076fb1a2097b6a6fc549a1a6490e8b2830
Rasha ali <rashaali84@gmail.com>
2020-06-01 18:50:59 +0200

    artist soltion api

--- api/artists/controllers.js
+++ api/artists/controllers.js
@@ -11,17 +11,81 @@ const controllers = {
         return;
       }
 
-      res.json(rows)
+      res.json(rows);
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+
+    const id = req.params.id;
+    const sql = `SELECT * FROM artists where ArtistID=${Number(id)}`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json(rows)
+    });
+
+  },
   create: (req, res) => {
     // read row data from body
+    const newObjString = JSON.stringify(req.body);
+    // console.log(req.body);
+    const newObj = JSON.parse(newObjString.toLocaleLowerCase());
+
+    console.log(newAlbum);
+    const sql = `INSERT INTO artists (
+                       name
+                   )values("${newObj.name}")`;
+
+    db.run(sql, function (err) {
+      if (err) {
+
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      //return the last id if the table 
+      res.json({ lastID: this.lastID });
+    });
+
   },
   update: (req, res) => {
     // read row data from body
+    const newObjString = JSON.stringify(req.body);
+    // console.log(req.body);
+    const newObj = JSON.parse(newObjString.toLocaleLowerCase());
+    const sql = `UPDATE artists
+    SET 
+     name = "${newObj.name}"
+    WHERE artistid = ${Number(req.params.id)}`;
+
+    db.run(sql, function (err) {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+      //return the last id if the table 
+      res.json({ changes: this.changes });
+    });
   },
-  delete: (req, res) => { }
+  delete: (req, res) => {
+
+    const sql = `DELETE FROM artists
+      WHERE artistid = ${Number(req.params.id)}`;
+
+    db.run(sql, function (err) {
+      if (err) {
+        res.status(400).json({ "error": err.message });
+        return;
+      }
+
+      res.json({ deleted: this.changes });
+    });
+
+  }
 }
 
 module.exports = controllers;
